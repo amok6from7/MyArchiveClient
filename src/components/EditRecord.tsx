89556b1d2c68f26ab6d86483b41a52023643ff0c
amok6from7 <kwifi7@gmail.com>
2020-05-20 08:46:26 +0900

    add suggest for edit

--- src/components/EditRecord.tsx
+++ src/components/EditRecord.tsx
@@ -1,11 +1,12 @@
 import React, { useLayoutEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom'
-import { useForm, ErrorMessage } from 'react-hook-form'
+import { useForm, ErrorMessage, Controller } from 'react-hook-form'
+import AsyncSelect from 'react-select/async'
 import axios from 'axios'
 
 export const EditRecord: React.FC = () => {
   const { record_id } = useParams()
-  const { register, errors, handleSubmit, setValue } = useForm()
+  const { register, errors, handleSubmit, setValue, control } = useForm()
   const [ isLoading, setIsLoading ] = useState(false)
   const [ message, setMessage ] = useState('')
 
@@ -29,8 +30,7 @@ export const EditRecord: React.FC = () => {
         { title: record[0].title }, 
         { title_kana: record[0].title_kana }, 
         { evaluation: record[0].evaluation }, 
-        { author: record[0].name }, 
-        { author_id: record[0].author_id }, 
+        { author: { value: record[0].author_id, label: record[0].name } },
       ])
     })
   }
@@ -41,7 +41,7 @@ export const EditRecord: React.FC = () => {
     params.append('title', data.title)
     params.append('title_kana', data.title_kana)
     params.append('evaluation', data.evaluation)
-    params.append('author_id', data.author_id)
+    params.append('author_id', data.author.value)
     const API_URL = `${process.env.REACT_APP_HEROKU_API}record/update`
     const response = await axios.post(API_URL, params).then((res) => {
       return res.data
@@ -51,9 +51,33 @@ export const EditRecord: React.FC = () => {
     } else {
       setMessage("データ更新に失敗しました")
     }
-    //console.log(response)
+    console.log(response)
   }
 
+  type authorOption = {
+    value: string,
+    label: string
+  }
+
+  const callApi = async (param: string) => {
+    console.log('call callapifunc')
+    console.log(typeof param)
+    let authors: authorOption[] = []
+    if (!param) return authors
+    const GET_AUTHOR_URL = `${API_URL}author/search?name=${param}`
+    await axios.get(GET_AUTHOR_URL).then(res => {
+      const response = res.data
+      console.log(response)
+      Array.from(response).forEach((e: any) => {
+        const author = {
+          value: e.ID,
+          label: e.Name
+        }
+        authors.push(author)
+      });
+    })
+    return authors
+  }
 
   return (
     <>
@@ -67,11 +91,14 @@ export const EditRecord: React.FC = () => {
           <input type="hidden" name="id" value={record_id} ref={register}/>
           Title：<input type="text" name="title" ref={register({ required: true })} />
           <ErrorMessage errors={errors} name="title" as="span" message="this is required"/><br/>
-          TitleKana：<input type="text" name="title_kana" ref={register({ required: true })}/><br/>
+          TitleKana：<input type="text" name="title_kana" ref={register}/><br/>
           Eval：<input type="text" name="evaluation" ref={register({ required: true })}/>
           <ErrorMessage errors={errors} name="evaluation" as="span" message="this is required"/><br/>
-          <input type="text" name="author" ref={register({ required: true })}/><br/>
-          <input type="hidden" name="author_id" ref={register}/><br/>
+          <Controller 
+            as={<AsyncSelect loadOptions={callApi}/>}
+            name="author"
+            control={control}
+          />
           <input type="submit" value="更新"/>
         </form>
       )}
