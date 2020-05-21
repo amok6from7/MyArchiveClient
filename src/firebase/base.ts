841f5abcc8c09ac485c0d3a18b0caa0f5b585116
amok6from7 <kwifi7@gmail.com>
2020-05-21 15:40:54 +0900

    fix import firebase

--- src/firebase/base.ts
+++ src/firebase/base.ts
@@ -1,7 +1,7 @@
 import 'firebase/auth'
 import 'firebase/firebase-firestore'
 
-import firebase from 'firebase'
+import * as firebase from 'firebase/app'
 
 const config = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
